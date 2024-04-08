38ef0dcf5195a8c13afa67a4058fe7b53f2abcbe
Harsh Sharma <148344142+swayamprogramator@users.noreply.github.com>
2024-04-08 19:58:40 +0530

    program

---
+++ pattern printing/inverted_halfpyramid.cpp
@@ -0,0 +1,18 @@
+//Calculator using Switch
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    int r,i=0;
+    cout<<"Enter row:";
+    cin>>r;
+    for(i=0;i<r;i++){
+        for(int j=0;j<=r-i-1;j++){
+            cout<<"* ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
