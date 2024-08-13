e5b0350fe998a28d6d065b5e51c81e62fc7d682f
Harsh Sharma <student3.ai@csvtu.ac.in>
2024-08-13 07:08:22 +0530

    Create leap_year_code.cpp

---
+++ leap_year_code.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int year;
+ 
+    cout << "Enter the year in yyyy format : ";
+    cin >> year;
+    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+        cout << year << " is a leap year";
+    else
+        cout << year << " is not a leap year";
+}
